f1d1798dfdfbe8f8c78caa69ed7c45ee4603cf23
Malibushko <malibushko@gmail.com>
2022-03-19 22:21:40 +0200

    *add return key handling

---
+++ SquirrelVS/SquirrelSyntaxHighlight/Editor/Typing/ReturnKeyCommandHandler.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Linq;
+using Microsoft.VisualStudio.Commanding;
+using Microsoft.VisualStudio.Text.Editor.Commanding.Commands;
+using Microsoft.VisualStudio.Text.Editor.OptionsExtensionMethods;
+using Microsoft.VisualStudio.Utilities;
+
+namespace SquirrelSyntaxHighlight.Editor.Typing
+{
+  [Export(typeof(ICommandHandler))]
+  [ContentType(SquirrelConstants.SquirrelContentType)]
+  [Name(nameof(ReturnKeyCommandHandler))]
+  internal class ReturnKeyCommandHandler : ICommandHandler<ReturnKeyCommandArgs>
+  {
+    public string DisplayName => nameof(ReturnKeyCommandHandler);
+
+    public CommandState GetCommandState(ReturnKeyCommandArgs args) => CommandState.Unspecified;
+
+    public bool ExecuteCommand(
+        ReturnKeyCommandArgs    _Args, 
+        CommandExecutionContext _ExecutionContext
+      )
+    {
+      var Point = _Args.TextView.Caret.Position.BufferPosition;
+
+      if (Point != null)
+      {
+        var Line         = Point.GetContainingLine();
+        var LineText     = Point.Snapshot.GetText(Line.Start, Point - Line.Start);
+        int CommentStart = LineText.IndexOf("//");
+        
+        if (CommentStart >= 0                                    &&
+            Point <= Line.End                                    &&
+            Line.Start + CommentStart < Point                    &&
+            string.IsNullOrWhiteSpace(LineText.Remove(CommentStart))
+          )
+        {
+          int Extra = LineText.Skip(CommentStart + 2).TakeWhile(char.IsWhiteSpace).Count() + 2;
+
+          using (var Edit = Line.Snapshot.TextBuffer.CreateEdit())
+          {
+            Edit.Insert(
+                Point.Position,
+                _Args.TextView.Options.GetNewLineCharacter() + LineText.Substring(0, CommentStart + Extra)
+              );
+
+            Edit.Apply();
+          }
+
+          return true;
+        }
+      }
+
+      return false;
+    }
+  } 
+}
